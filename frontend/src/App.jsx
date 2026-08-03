2f4f7d52703b85a0a79597e0d1d68d12992f8d7a
Humancare11 <webdev@humancareworldwidecom>
2026-08-03 17:01:14 +0530

    telehealth-1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -230,7 +230,7 @@ const PediatricColdFlu = lazy(
 );
 const PediatricFever = lazy(() => import("./pages/Conditions/PediatricFever"));
 const PinkEyeChildren = lazy(
-  () => import("./pages/Conditions/PinkEyeChildren")f,
+  () => import("./pages/Conditions/PinkEyeChildren"),
 );
 const PubertyConcerns = lazy(
   () => import("./pages/Conditions/PubertyConcerns"),
